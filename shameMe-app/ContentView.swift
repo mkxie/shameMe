ad0ce206ab7ae9778ccfaf04fb49566edc942bde
Matthew Xie <mkxie@berkeley.edu>
2020-11-30 03:02:16 -0800

    Initial Commit

---
+++ shameMe-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shameMe-app
+//
+//  Created by Matthew Xie on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shameMe-app/shameMe_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  shameMe_appApp.swift
+//  shameMe-app
+//
+//  Created by Matthew Xie on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct shameMe_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
